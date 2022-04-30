a554dff56b59b1a666e9e298dcea8cb30b3fe151
Desh Bagley <stemtrainer@gmail.com>
2022-04-30 19:28:56 -0400

    JSX Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import About from "./About.js";
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
 
 function App() {
   return (
